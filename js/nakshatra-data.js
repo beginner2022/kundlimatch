1a0da4abc310dd51d7fb8720cdc4b04c58fdf37c
beginner2022 <94739853+beginner2022@users.noreply.github.com>
2026-06-22 21:41:29 +0530

    Create nakshatra-data.js

---
+++ js/nakshatra-data.js
@@ -0,0 +1,36 @@
+const nakshatraIndex = {
+"Ashwini": 1,
+"Bharani": 2,
+"Krittika": 3,
+"Rohini": 4,
+"Mrigashira": 5,
+"Ardra": 6,
+"Punarvasu": 7,
+"Pushya": 8,
+"Ashlesha": 9,
+"Magha": 10,
+"Purva Phalguni": 11,
+"Uttara Phalguni": 12,
+"Hasta": 13,
+"Chitra": 14,
+"Swati": 15,
+"Vishakha": 16,
+"Anuradha": 17,
+"Jyeshtha": 18,
+"Mula": 19,
+"Purva Ashadha": 20,
+"Uttara Ashadha": 21,
+"Shravana": 22,
+"Dhanishta": 23,
+"Shatabhisha": 24,
+"Purva Bhadrapada": 25,
+"Uttara Bhadrapada": 26,
+"Revati": 27
+};
+
+// simple compatibility table (starter logic)
+const baseCompatibility = {
+  good: [1,2,3,4,5,6,7,8,9],
+  average: [10,11,12,13,14,15,16,17,18],
+  low: [19,20,21,22,23,24,25,26,27]
+};
